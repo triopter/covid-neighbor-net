713901984911444ed6e6001e498e6ab9020629b9
Noemi Millman <noemi@triopter.com>
2020-03-18 16:37:27 -0400

    prelim models and migrations

--- engagements/models.py
+++ engagements/models.py
@@ -1,27 +1,38 @@
 from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from neighbors.models import Address
 
 
-STATUSES = Choices(
-    # requested
-    # claimed
-    # completed
-)
+class EngagementStatus(models.TextChoices):
+    REQUESTED = ('requested', _('Requested'))
+    CLAIMED = ('claimed', _('Claimed'))
+    COMPLETED = ('completed', _('Completed'))
 
 
 class Engagement(models.Model):
-    requester = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='requested_engagements')
+    requester = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='requested_engagements'
+    )
     submitted_at = models.DateTimeField(auto_now_add=True, blank=True)
     expires = models.DateTimeField(
-        blank=True
-    )  # allow entry, or if omitted, set automatically to N days after submission
-    address = models.ForeignKey(Address, related_name='engagements')
+        blank=True,
+        help_text=_(
+            "If omitted, will be set to submission timestamp plus the value specified in settings.DEFAULT_ENGAGEMENT_EXPIRATION",
+        ),
+    )
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='engagements')
     description = models.TextField()
-    status = models.CharField(choices=STATUSES, max_length=20)
+    status = models.CharField(
+        choices=EngagementStatus.choices, max_length=20, default=EngagementStatus.REQUESTED
+    )
     claimed_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name='claimed_engagements', null=True, blank=True
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='claimed_engagements',
     )
     claimed_at = models.DateTimeField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
--- neighbors/models.py
+++ neighbors/models.py
@@ -1,52 +1,63 @@
 from django.conf import settings
 from django.contrib.gis.db import models as geo_models
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+# @TODO: we may eventually want to use Django-Cities or the like for this instead, but let's start simple
+from localflavor.us.us_states import USPS_CHOICES
+from localflavor.ca.ca_provinces import PROVINCE_CHOICES
 
 # We may want this data in fixtures or migrations to initially seed the DB
 ROLES = [
     # Volunteer
     # Requester
+    # Dispatcher -- someone who accepts calls instead of emails from less tech-savvy participants
     # Moderator
 ]
 
 
 class Role(models.Model):
-    name = models.CharField(
-        max_length=50
-    )  # It seems like in theory someone could switch between roles -- volunteer but eventually have to self-quarantine or vice-versa
+    name = models.CharField(max_length=50)
 
 
-class NeighborProfile(geo_models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+class NeighborProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile'
+    )
     given_name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100, blank=True)
-    email = models.EmailField(blank=True)  # blankable because elderly folks may not have one
-    phone = models.CharField(
-        max_length=50, blank=True
-    )  # should this actually be blankable for privacy reasons?  Maybe require that the user provide at least one of email or phone?  Do we need to support multiple phone numbers per person?
+    email = models.EmailField(
+        blank=True,
+        help_text=_(
+            "Required to create an account via front end, but allowed to be omitted by dispatchers creating requests on behalf of other neighbors"
+        ),
+    )
+    phone = models.CharField(max_length=50, blank=True)
     roles = models.ManyToManyField(Role, related_name='profiles')
 
     # @TODO: vulnerability level
     # @TODO: financial need
 
+    # @TDOD: verify that either email or phone is provided
 
-class Address(models.Model):
-    owner = models.ForeignKey(
-        NeighborProfile, related_name='addresses'
-    )  # doing this  as a relation because a) then it's OK for volunteers to input a pin drop instead of an address, and b) it's conceivable that we'd want or need multiple addresses for someone
 
-    # This is one part that's going to be a pain to internationalize while also supporting geocoding
+class Address(geo_models.Model):
+    owner = models.ForeignKey(NeighborProfile, on_delete=models.CASCADE, related_name='addresses')
+
+    # @TODO: i18n-able version of this
     address_line_1 = models.CharField(max_length=100)
     address_line_2 = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=100)
-    state_or_province = models.ForeignKey(States)  # or call this 'region'?
+    region = models.CharField(
+        max_length=100,
+        choices=(USPS_CHOICES + PROVINCE_CHOICES),
+        help_text=_("Starting with US State / Canadian province"),
+    )
     postal_code = models.CharField(max_length=30)
 
-    lat = models.FloatField(
-        blank=True, null=True
-    )  # nullable so we can save an address even if we have trouble geocoding it
-    lng = models.FloatField(blank=True, null=True)
+    lng_lat = geo_models.PointField(
+        geography=True, null=True, help_text=_("Note that longitude comes first")
+    )  # nullable so we can store address even if we can't geocode it
 
     def save(self, *args, **kwargs):
         # @TODO: geocode if not yet geocoded
